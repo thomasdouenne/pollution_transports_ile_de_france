6768556e5dfae3abf52641b9175635643ad85989
Thomas DOUENNE <t.douenne@pse.local>
2018-07-09 15:19:23 +0200

    Read data from survey EGT, wip

---
+++ read_data_egt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 15:09:33 2018
+
+@author: t.douenne
+"""
+
+import numpy as np
+import pandas as pd
+df = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\deplacements_semaine.dta')
