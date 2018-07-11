d777c54eea27c8b3394b27842329bd072e1f98a3
Thomas DOUENNE <t.douenne@pse.local>
2018-07-11 12:15:23 +0200

    Build complete database

--- read_data_egt.py
+++ read_data_egt.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Pour le moment rien ne fonctionne, à part la concaténation des différents jours de la semaine
-# A partir de la doc, faire le tri dans les variables et surtout éviter de les cumuler lors du merge
-
-
-
-
 from __future__ import division
 
 
@@ -18,15 +12,105 @@ df_menages_samedi = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\e
 df_menages_dimanche = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\menages_dimanche.dta')
 
 df_menages = pd.concat([df_menages_semaine, df_menages_samedi, df_menages_dimanche])
-df_menages = df_menages.drop_duplicates(subset=['nquest'], keep=False)
+assert(len(df_menages) == len(df_menages.drop_duplicates(subset=['nquest'], keep=False)))
+del df_menages_semaine, df_menages_samedi, df_menages_dimanche
+
 
 # Selection des variables ménages
 variables_menages = [
-    'nquest',
+    'nquest', # id_menage
+    'sem', # semaine enquête
+    'poidsm', # poids ménage
+    'rescour', # couronne de résidence
+    'resdep',# département de résidence
+    'ressect', # secteur de résidence
+    'rescomm', # commune de résidence
+    'resc', # carreau de résidence
+    'jdep', # jour des déplacements
+    'mnp', # nombre personnes ménage
+    'mnp5', # nombre 5 ans et plus
+    'mnpmob', # nombre qui se sont déplacés
+    'mnpact', # nombre actifs
+    'typelog', # type de logement
+    'occuplog', # occupation du logement
+    'loy_hc', # montant du loyer hors charge
+    'alloc', # allocations
+    'chg', # montant des charges mensuelles
+    'loy_park', # montant des loyers de parking
+    'nbpi', # nombre de pièces
+    'surf', # superficie totale logement
+    'nb_velo', # nb vélos en état de marche
+    'nb_vae', # nombre de vae ????
+    'nb_vd', # nombre de voitures à disposition
+    'typev1', # type du véhicule 1
+    'energv1', # énergie du véhicule
+    'apmcv1', # année de première mise en circulation
+    'puissv1', # puissance fiscale
+    'ankmv1', # kilométrage moyen annuel
+    'cptkmv1', # kilométrage au compteur
+    'possv1', # possession du véh 1
+    'statv1', # stationnement la nuit véh 1
+    'typev2',
+    'energv2',
+    'apmcv2',
+    'puissv2',
+    'ankmv2',
+    'cptkmv2',
+    'possv2',
+    'statv2',
+    'typev3',
+    'energv3',
+    'apmcv3',
+    'puissv3',
+    'ankmv3',
+    'cptkmv3',
+    'possv3',
+    'statv3',
+    'typev4',
+    'energv4',
+    'apmcv4',
+    'puissv4',
+    'ankmv4',
+    'cptkmv4',
+    'possv4',
+    'statv4',
+    'nb_2rm', # nb de 2rm ou 3rm à disposition
+    'nb_veh', # nb veh motorisés à disposition
+    'typerm1', # type du 2rm/3rm 1
+    'motrm1', # type de moteur
+    'enerm1', # type d'énergie
+    'apmcrm1', # année mise en circulation
+    'cylrm1', # cylindrée
+    'ankmrm1', # km moyen annuel
+    'statrm1', # stationnement la nuit
+    'typerm2',
+    'motrm2',
+    'enerm2',
+    'apmcrm2',
+    'cylrm2',
+    'ankmrm2',
+    'statrm2',
+    'typerm3',
+    'motrm3',
+    'enerm3',
+    'apmcrm3',
+    'cylrm3',
+    'ankmrm3',
+    'statrm3',
+    'typerm4',
+    'motrm4',
+    'enerm4',
+    'apmcrm4',
+    'cylrm4',
+    'ankmrm4',
+    'statrm4',
+    'ancout', # cout annuel entretien/réparation flotte de veh
+    'asscout', # cout annuel assurance
+    'revenu', # classe de revenu net mensuel
     ]
 
 df_menages = df_menages[variables_menages]
-
+del variables_menages
 
 # Données table personnes :
 df_personnes_semaine = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\personnes_semaine.dta')
@@ -34,15 +118,63 @@ df_personnes_samedi = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt
 df_personnes_dimanche = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\personnes_dimanche.dta')
 
 df_personnes = pd.concat([df_personnes_semaine, df_personnes_samedi, df_personnes_dimanche])
+del df_personnes_semaine, df_personnes_samedi, df_personnes_dimanche
 
 # Selection des variables personnes
 variables_personnes = [
     'nquest',
-    'np'
+    'np',
+    'typep', # type de personne
+    'sexe',
+    'lienpref', # lien avec personne de référence,
+    'age',
+    'trage', # classe d'âge
+    'permvp', # permis voiture
+    'abonvp', # abonnement autopartage
+    'perm2rm', # permis 2rm
+    'abontc', # abonnement tc
+    'zonitc', # zonage de 'labonnement tc (première zone)
+    'zonftc', # zonage de 'labonnement tc (dernière zone)
+    'supptc', # support abonnement
+    'rembtc', # % de l'abonnement à charge
+    'abonvls', # abonnement vélib ou vls
+    'dipl', # niveau d'étude atteint
+    'occp', # occupation principale
+    'typlt', # type lieu de travail
+    'cs24l', # cat socio-pro
+    'cs8', # cat socio-pro en 8 postes
+    'cat', # categories de personne
+    'ultrav', # unicité lieu de travail/étude
+    'ltravcour', # couronne lieu de travail/études
+    'ltravdep', # département
+    'ltravsect', # secteur
+    'ltravcomm', # commune
+    'ltravc', # carreau
+    'pkvptrav', # disponibilité parking lieu de travail/étude
+    'pkvltrav', # dispo parking vélo
+    'handi', # gêne dans les déplacements en général
+    'handi1', # type de gène 1
+    'handi2',
+    'handi3',
+    'handi4',
+    'conge', # conge, arrête de maladie
+    'nondepl', # déplacement ou non hier
+    'perturb', # pertubations pour les déplacements
+    'tperturb', # type de perturbation
+    'gene', # gene temporaire ou permanente pour la journée enquêtée
+    'puvp', # possibilité d'utiliser un véhicule motorisé conducteur ?????
+    'nbdepl', # nb déplacements réalisés
+    'nbdeplvp', # nb en voiture
+    'nbdeplvpc', # nb en voiture en tant que conducteur
+    'nbdepltc', # nb en transports collectifs
+    'nbdeplvelo', # nb à vélo
+    'nbdepl2rm', # nb à 2 roues motorisé
+    'nbdeplmap', # nb à pied
+    'ddomtrav', # portée du déplacement domicile-travail/étude en km
     ]
 
 df_personnes = df_personnes[variables_personnes]
-
+del variables_personnes
 
 # Données table déplacements :
 df_deplacements_semaine = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\deplacements_semaine.dta')
@@ -50,16 +182,58 @@ df_deplacements_samedi = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_
 df_deplacements_dimanche = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\deplacements_dimanche.dta')
 
 df_deplacements = pd.concat([df_deplacements_semaine, df_deplacements_samedi, df_deplacements_dimanche], sort = False)
+del df_deplacements_semaine, df_deplacements_samedi, df_deplacements_dimanche
 
 # Selection des variables déplacements
 variables_deplacements = [
     'nquest',
     'np',
-    'nd'
+    'nd',
+    'orcour', # couronne d'origine du déplacement
+    'ordep', # departement
+    'orsect', # secteur
+    'orcomm', # commune
+    'orc', # carreau
+    'orh', # heure de départ
+    'orm', # minute de départ
+    'ormot', # motif au départ
+    'ormot_h9', # motif, 9 catégories
+    'destcour', # couronne de destination
+    'destdep', # département
+    'destsect',
+    'destcomm',
+    'destc',
+    'desth',
+    'destm',
+    'destmot',
+    'destmot_h9',
+    'destmot_iaurif02', # motif à l'arrivée
+    'motif_combine', # motif combiné origine/destination
+    'dportee', # portée du déplacement en km
+    'duree', # durée en minutes
+    'tlt', # type du lieu de travail si dest = lieu de travail
+    'tla', # type lieu d'achat si...
+    'nbat', # nb d'arrêts dans la tournée
+    'trp', # avez vous traversé Paris intra muros pendant le déplacement ?
+    'modp_strict', # mode principal strict détaillé
+    'modp_h6', #C mode pirncipal, 6 modalités
+    'modp_h7',
+    'modp_h12',
+    'modp_h19',
+    'nbtraj', # nb trajets
+    'nbtrajvp', # nb en voiture
+    'nbtrajvpc', # en tant que conducteur
+    'nbtrajtc', # transports collectifs
+    'nbtrajvelo', # nb en vélo
+    'nbtraj2rm',
+    'idm', # indicateur de déplacement motorisé
+    'nbco', # nb de correspondances
+    'rab_tc', # mode principal rabattement vers les TC
+    'diff_tc' # mode principal diffusion depuis les TC
     ]
 
 df_deplacements = df_deplacements[variables_deplacements]
-
+del variables_deplacements
 
 # Données table trajets :
 df_trajets_semaine = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\trajets_semaine.dta')
@@ -67,35 +241,62 @@ df_trajets_samedi = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\e
 df_trajets_dimanche = pd.read_stata(r'C:\Users\t.douenne\Documents\Data\data_egt\egt_2010\Stata\trajets_dimanche.dta')
 
 df_trajets = pd.concat([df_trajets_semaine, df_trajets_samedi, df_trajets_dimanche])
+del df_trajets_semaine, df_trajets_samedi, df_trajets_dimanche
 
 # Selection des variables trajets
 variables_trajets = [
     'nquest',
     'np',
-    'nd'
+    'nd',
+    'nt',
+    'moyen', # moyen utilisé pour le trajet
+    'tt', # titre de transport utilisé
+    'ligne', # ligne de TC utilisée
+    'entc', # carreau d'entrée
+    'sortc', # carreau de sortie
+    'tportee', # portee du trajet en km
+    'typv', # num du veh du menage utilisé
+    'tstat', # type de stationnement
+    'nbpv', # nb personnes dans le veh
+    'utp', # emprunté le périph
+    'uta86', # emprunté l'A86
+    'utfrl', # emprunté la francilienne
+    'rembf', # remboursement frais véhicule par employeur
+    'pct', # cout du trajet à charge
+    'entsect', # secteur entrée dans le mode
+    'sortsect', # secteur de sortie
     ]
 
 df_trajets = df_trajets[variables_trajets]
+del variables_trajets
 
-
-# Revoir tout ce qui suit...
 
 # Données ménages - personnes
 df_menages_personnes = df_personnes.merge(df_menages, on = 'nquest')
+assert(len(df_menages_personnes) == len(df_personnes))
+assert(len(df_menages_personnes.columns) == len(df_menages.columns) + len(df_personnes.columns) - 1)
 
-df_trajets['np'] = str(df_trajets['np'])
-df_deplacements['np'] = str(df_deplacements['np'])
-df_trajets['nd'] = str(df_trajets['nd'])
-df_deplacements['nd'] = str(df_deplacements['nd'])
+df_menages_personnes['np'] = df_menages_personnes['np'].astype(str, inplace = True)
+df_menages_personnes['id_personne'] = df_menages_personnes['nquest'] + '_' + df_menages_personnes['np']
+
+# Données déplacements - trajets
+df_trajets['np'] = df_trajets['np'].astype(str, inplace = True)
+df_trajets['nd'] = df_trajets['nd'].astype(str, inplace = True)
 df_trajets['id_deplacement'] = df_trajets['nquest'] + '_' + df_trajets['np'] + '_' + df_trajets['nd']
-df_trajets = df_trajets.drop(columns=['nquest', 'np', 'nd'])
+df_trajets = df_trajets.drop(['nquest', 'np', 'nd'], axis=1)
+
+
+df_deplacements['np'] = df_deplacements['np'].astype(str, inplace = True)
+df_deplacements['nd'] = df_deplacements['nd'].astype(str, inplace = True)
 df_deplacements['id_deplacement'] = df_deplacements['nquest'] + '_' + df_deplacements['np'] + '_' + df_deplacements['nd']
+df_deplacements['id_personne'] = df_deplacements['nquest'] + '_' + df_deplacements['np']
 df_deplacements = df_deplacements.drop(columns=['nquest', 'np', 'nd'])
 
-df_deplacements_trajets = df_deplacements.merge(df_trajets, on = 'id_deplacements')
+df_deplacements_trajets = df_deplacements.merge(df_trajets, on = 'id_deplacement')
+assert(len(df_deplacements_trajets) == len(df_trajets))
+assert(len(df_deplacements_trajets.columns) == len(df_deplacements.columns) + len(df_trajets.columns) - 1)
 
-df_menages_personnes['np'] = str(df_menages_personnes['np'])
-df_menages_personnes['id_personne'] = df_menages_personnes['nquest'] + '_' + df_menages_personnes['np']
-df_deplacements_trajets['id_personne'] = df_deplacements_trajets['id_deplacement'].str[:-2]
-
+# Base finale
 df_finale = df_menages_personnes.merge(df_deplacements_trajets, on = 'id_personne')
+assert(len(df_finale) == len(df_deplacements_trajets))
+assert(len(df_finale.columns) == len(df_deplacements_trajets.columns) + len(df_menages_personnes.columns) - 1)
