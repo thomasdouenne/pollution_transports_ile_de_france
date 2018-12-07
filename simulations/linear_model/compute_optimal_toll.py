5c703d0c7e81cde3b2ed1a401506c331d863f8f0
thomas <thomas.douenne@sciencespo.fr>
2018-12-07 16:10:29 +0100

    Redefine optimal toll as maximizing surplus share

--- simulations/linear_model/compute_optimal_toll.py
+++ simulations/linear_model/compute_optimal_toll.py
@@ -9,7 +9,7 @@ seaborn.set_palette(seaborn.color_palette("Set2", 12))
 
 """
 In this script, we compute the optimal toll in the linear model with revenue-recycling
-An important assumption is that homes are owned by people within the region so that the aggregate welfare impact of rents is null
+An important assumption is that agents own their homes so that the aggregate welfare impact of rents is null
 """
 
 
@@ -33,7 +33,7 @@ theta = (1.0 / 155) * 40 * (22.0 / 60) * 0.5 # People spend 25min in their car w
 # they do the trip 40 times a month, their vtt is 0.5 their income
 mu = 3.5 # sensitivity of time travel to congestion
 gamma = 3 # sensitivity of time travel to congestion
-epsilon = 0.02
+epsilon = 0.025
 a = -100 # amenity value in euro per month
 p_v = 4.5 * 40 # price of the trip in PV, for a month
 p_t = 1 * 40 # Price of the trip in TC, for a month
@@ -81,6 +81,7 @@ df_sim['r_'] = lower_r + df_sim['index'] # Check that the distributon is consist
 df_optimal = pd.DataFrame(index = range(0,101),
     columns = ['toll', 'aggregate_surplus']
     )
+max_aggregate = 0
 for toll in range(0,101):
     monthly_toll = float(toll) / 10 * 40
     df_sim['r_after_toll'] = df_sim['r_'] + df_toll['transfer'][toll]
@@ -105,7 +106,19 @@ for toll in range(0,101):
         + df_sim['b_v'] * (df_sim['b_v'] + 1e-06 > df_sim['b_d']) * (df_sim['b_v'] + 1e-06 > df_sim['b_t'])
         + df_sim['b_t'] * (df_sim['b_t'] > df_sim['b_d']) * (df_sim['b_t'] > df_sim['b_v'] + 1e-06)
         )
+    if toll == 0:
+        df_sim['b_option_default'] = df_sim['b_option']
+        df_optimal['aggregate_surplus'][toll] = 0
+    else:
+        df_sim['gain_surplus'] = (
+            (df_sim['b_option'] - df_sim['b_option_default']) / df_sim['b_option_default']  
+            ) * 100
+        df_optimal['aggregate_surplus'][toll] = float(df_sim['gain_surplus'].sum()) / len(df_sim)
 
     df_optimal['toll'][toll] = toll
-    df_optimal['aggregate_surplus'][toll] = float(df_sim['b_option'].sum()) / len(df_sim)
-    
+    aggregate_surplus = df_optimal['aggregate_surplus'][toll]
+    if aggregate_surplus > max_aggregate:
+        optimal_tax = toll
+    max_aggregate = max(aggregate_surplus, max_aggregate)
+
+print "The optimal toll is {} euros per trip".format(float(optimal_tax) / 10)
